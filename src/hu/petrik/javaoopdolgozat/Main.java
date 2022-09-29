a4914d4a6acd2701a2a8d69f6df7ac7421f5a50c
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-29 08:54:28 +0200

    2. feladat - b kész van első lista feltöltése bekért értékekkel

--- src/hu/petrik/javaoopdolgozat/Main.java
+++ src/hu/petrik/javaoopdolgozat/Main.java
@@ -2,6 +2,7 @@ package hu.petrik.javaoopdolgozat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -19,6 +20,24 @@ public class Main {
         List<Bejegyzes> lista1 = new ArrayList<>();
         List<Bejegyzes> lista2 = new ArrayList<>();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adjon meg egy darabszámot: ");
+        int darabSzam = sc.nextInt();
+        if (darabSzam < 0){
+            System.out.println("Természetes számot adj meg!");
+        }
+        String tempSzerzo = "";
+        String tempTartalom = "";
+        for (int i = 0; i < darabSzam; i++){
+            System.out.print("Adja meg a(z) " + (i+1) + ". bejegyzés szerzőjét: ");
+            tempSzerzo = sc.next();
+            System.out.print("Adja meg a(z) " + (i+1) + ". bejegyzés tartalmát: ");
+            tempTartalom = sc.next();
+            lista1.add(new Bejegyzes(tempSzerzo, tempTartalom));
+        }
+        for (Bejegyzes b : lista1){
+            System.out.println(b);
+        }
 
     }
 }
