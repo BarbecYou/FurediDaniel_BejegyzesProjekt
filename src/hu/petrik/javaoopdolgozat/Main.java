1b11eb9524eca34b35807cbc0a7d0a3d00191bf7
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-29 10:12:17 +0200

    2. feladat - c kész van file beolvasasa es elemek hozzaadas listahoz

--- src/hu/petrik/javaoopdolgozat/Main.java
+++ src/hu/petrik/javaoopdolgozat/Main.java
@@ -1,24 +1,42 @@
 package hu.petrik.javaoopdolgozat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    public static List<Bejegyzes> lista1 = new ArrayList<>();
+    public static List<Bejegyzes> lista2 = new ArrayList<>();
+
     public static void main(String[] args) {
-	    Bejegyzes bejegyzes = new Bejegyzes("Füredi Dániel", "lorem ipsum sjsahskahgioaghgahhgigas");
 
-        bejegyzes.like();
-        bejegyzes.like();
-        bejegyzes.like();
+        //osztalyTesztelesFeladat();
+        //listaFeltoltesFeladat();
+        fileBeolvasFeladat();
+    }
 
-        bejegyzes.setTartalom("lorem ipsum alma");
+    private static void fileBeolvasFeladat() {
 
-        System.out.println(bejegyzes);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("bejegyzesek.csv"));
+        } catch (FileNotFoundException e){
+            System.out.println("error");
+        }
+        while (sc.hasNext()){
+            String[] temp = sc.nextLine().split(";");
+            lista2.add(new Bejegyzes(temp[0], temp[1]));
+        }
+        for (Bejegyzes b : lista2){
 
-        List<Bejegyzes> lista1 = new ArrayList<>();
-        List<Bejegyzes> lista2 = new ArrayList<>();
+            System.out.println(b);
+        }
+    }
+
+    private static void listaFeltoltesFeladat(){
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Adjon meg egy darabszámot: ");
@@ -26,8 +44,8 @@ public class Main {
         if (darabSzam < 0){
             System.out.println("Természetes számot adj meg!");
         }
-        String tempSzerzo = "";
-        String tempTartalom = "";
+        String tempSzerzo;
+        String tempTartalom;
         for (int i = 0; i < darabSzam; i++){
             System.out.print("Adja meg a(z) " + (i+1) + ". bejegyzés szerzőjét: ");
             tempSzerzo = sc.next();
@@ -38,6 +56,17 @@ public class Main {
         for (Bejegyzes b : lista1){
             System.out.println(b);
         }
+    }
 
+    private static void osztalyTesztelesFeladat(){
+        Bejegyzes bejegyzes = new Bejegyzes("Füredi Dániel", "lorem ipsum sjsahskahgioaghgahhgigas");
+
+        bejegyzes.like();
+        bejegyzes.like();
+        bejegyzes.like();
+
+        bejegyzes.setTartalom("lorem ipsum alma");
+
+        System.out.println(bejegyzes);
     }
 }
