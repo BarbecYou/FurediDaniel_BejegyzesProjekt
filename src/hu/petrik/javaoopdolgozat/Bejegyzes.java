d82d7831e1d9561e09df31252044cd4bdb5635f8
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-29 08:29:27 +0200

    1. feladat - a,b kész van Bejegyzes osztály létrehozása, adattagok, konstruktor, getterek, setterek, metódusok megadása

--- src/hu/petrik/javaoopdolgozat/Bejegyzes.java
+++ src/hu/petrik/javaoopdolgozat/Bejegyzes.java
@@ -1,4 +1,49 @@
 package hu.petrik.javaoopdolgozat;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+
+    String szerzo;
+    String tartalom;
+    int likeok;
+    LocalDateTime letrejott;
+    LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        this.likeok++;
+    }
 }
